300a7fd33d996e75be71d0f863bd3e298d1d50c0
Alif Muqri Hazmi <alifmuqri.hazm@petronas.com>
2024-03-15 09:49:06 +0800

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,14 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'inside',
+    loadChildren: () => import('./pages/inside/inside.module').then( m => m.InsidePageModule)
+  },
 ];
 
 @NgModule({
