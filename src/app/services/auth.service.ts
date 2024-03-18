654663429df997850faaa34d732e5657b60452fb
Alif Muqri Hazmi <alifmuqri.hazm@petronas.com>
2024-03-18 11:25:26 +0800

    implement authentication service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,106 @@
+import { Platform, AlertController } from '@ionic/angular';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { Storage } from '@ionic/storage';
+import { environment } from '../../environments/environment';
+import { tap, catchError } from 'rxjs/operators';
+import { BehaviorSubject } from 'rxjs';
+
+const TOKEN_KEY = 'access_token';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  //import url env
+  url = environment.url;
+  user = null;
+  //auth state is a special type of observable where we can emit new values to all subscribers
+  authenticationState = new BehaviorSubject(false);
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    private helper: JwtHelperService,
+    private storage: Storage,
+    private plt: Platform,
+    private alertController: AlertController
+  ) {
+    this.plt.ready().then(() => {
+      this.checkToken();
+    });
+  }
+
+  checkToken() {
+    this.storage.get(TOKEN_KEY).then((token) => {
+      if (token) {
+        let decoded = this.helper.decodeToken(token);
+        let isExpired = this.helper.isTokenExpired(token);
+
+        if (!isExpired) {
+          this.user = decoded;
+          this.authenticationState.next(true);
+        } else {
+          this.storage.remove(TOKEN_KEY);
+        }
+      }
+    });
+  }
+
+  register(credentials: any) {
+    return this.http.post(`${this.url}/api/register`, credentials).pipe(
+      catchError((e) => {
+        this.showAlert(e.error.msg);
+        throw new Error(e);
+      })
+    );
+  }
+
+  login(credentials: any) {
+    return this.http.post(`${this.url}/api/login`, credentials).pipe(
+      tap((res: any) => {
+        //set token to storage
+        this.storage.set(TOKEN_KEY, res['token']);
+        //decode the user token and assign to user. later use user to get user info such as username
+        this.user = this.helper.decodeToken(res['token']);
+        this.authenticationState.next(true);
+      }),
+      catchError((e) => {
+        this.showAlert(e.error.msg);
+        throw new Error(e);
+      })
+    );
+  }
+
+  logout() {
+    this.storage.remove(TOKEN_KEY).then(() => {
+      this.authenticationState.next(false);
+    });
+  }
+
+  getSpecialData() {
+    return this.http.get(`${this.url}/api/special`).pipe(
+      catchError((e) => {
+        let status = e.status;
+        if (status === 401) {
+          this.showAlert('You are not authorized for this!');
+          this.logout();
+        }
+        throw new Error(e);
+      })
+    );
+  }
+
+  isAuthenticated() {
+    return this.authenticationState.value;
+  }
+
+  showAlert(msg: any) {
+    let alert = this.alertController.create({
+      message: msg,
+      header: 'Error',
+      buttons: ['OK'],
+    });
+    alert.then((alert) => alert.present());
+  }
 }
